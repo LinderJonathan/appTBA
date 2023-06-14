739f83b3fa4d41171711612224a77fa16b3466bf
LinderJonathan <linderjonathan3@gmail.com>
2023-06-15 00:51:33 +0200

    ignore

---
+++ DiscordBot/DiscordBot/botHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class botHandler
+    {
+        public async Task handleCommand(Bot bot, string command)
+        {
+
+        }
+    }
+}
