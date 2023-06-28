5753dac83f48bc5ef9fe16c09308711e0263f4d7
Jonathan Linder <linderjonathan3@gmail.com>
2023-06-28 16:41:41 +0200

    Unpushed changes not registrered?

---
+++ DiscordBot/DiscordBot/Commands/testCommands.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands
+{
+    public class testCommand : BaseCommandModule
+    {
+        [Command("ping")]
+        public async Task TestCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("bot pinged");
+        }
+        [Command("who")]
+        public async Task WhoCommand(CommandContext ctx)
+        {
+            var user = ctx.User;
+            await ctx.Channel.SendMessageAsync("Your username is :" + user.Username);
+        }
+    }
+}
---
+++ DiscordBot/DiscordBot/Commands/weatherCommands.cs
@@ -0,0 +1,19 @@
+﻿using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//TODO: a lot. Work on the SMHI API for C#
+namespace DiscordBot.Commands
+{
+    internal class weather : BaseCommandModule
+    {
+        public async Task weatherToday(CommandContext ctx, string city)
+        {
+
+        }
+    }
+}
